78687b3afca0afbc82b32b33035707fb51b6dc4b
fvrings <fvrings@proton.me>
2025-03-28 10:36:51 +0800

    try settings option

--- overlay/default.nix
+++ overlay/default.nix
@@ -3,6 +3,5 @@
   nixpkgs.overlays = [
     inputs.nur.overlays.default
     inputs.rust-overlay.overlays.default
-    inputs.niri.overlays.niri
   ];
 }
--- system/desktop/niri.nix
+++ system/desktop/niri.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   programs.niri = {
     enable = true;
-    package = pkgs.niri-unstable;
-    config = builtins.readFile ./config.kdl;
+    # config = builtins.readFile ./config.kdl;
+    settings.binds = {
+      "XF86AudioRaiseVolume".action.spawn =
+        [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
+      "XF86AudioLowerVolume".action.spawn =
+        [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
+    };
   };
 }
