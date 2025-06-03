d6856ca5e85a9d5e74270bfced6c901e0d136ef0
fvrings <fvrings@proton.me>
2025-06-03 15:19:50 +0800

    f
    fix cpu hang

--- system/service/default.nix
+++ system/service/default.nix
@@ -5,4 +5,8 @@
     # ./nextcloud.nix
     # ./ocis.nix
   ];
+  # NOTE: the southbridge is fucked in this host
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="on"
+  '';
 }
