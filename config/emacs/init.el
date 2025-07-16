13e32c8dc8ef2db7cfff219165062865a16b78dc
fvrings <fvrings@proton.me>
2025-07-16 12:54:45 +0800

    exwm

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -63,7 +63,6 @@
   (elpaca-use-package-mode))
 
 
-(package-initialize)
 (setq org-directory (file-truename "~/notes/eorg/"))
 (set-face-attribute 'default nil :height 200)
 (electric-pair-mode t)                       ; 自动补全括号
@@ -816,3 +815,29 @@
   (meow-setup)
   :init
   (meow-global-mode 1))
+
+(use-package exwm
+  :ensure t
+  :init
+  ;; Set the initial workspace number.
+  (setq exwm-workspace-number 4)
+  ;; Make class name the buffer name.
+  (add-hook 'exwm-update-class-hook
+	    (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
+  ;; Global keybindings.
+  (setq exwm-input-global-keys
+	`(([?\s-r] . exwm-reset) ;; s-r: Reset (to line-mode).
+          ([?\s-w] . exwm-workspace-switch) ;; s-w: Switch workspace.
+          ([?\s-&] . (lambda (cmd) ;; s-&: Launch application.
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command cmd nil cmd)))
+          ;; s-N: Switch to certain workspace.
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+			(lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))))
+  ;; Enable EXWM
+  (exwm-wm-mode)
+  (add-hook 'after-init-hook #'exwm-randr-mode))
