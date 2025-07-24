79406eeeccca83b51e69f919376d5b1c879ece89
fvrings <fvrings@proton.me>
2025-07-24 13:49:44 +0800

    update nvim-treesitter to main branch

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,153 +1,140 @@
-local function config()
-  if vim.g.iswin then
-    require('nvim-treesitter.install').compilers = { 'gcc' }
-  end
-  ---@diagnostic disable-next-line: missing-fields
-  require('nvim-treesitter.configs').setup {
-    -- ignore_install = {},
-    ensure_installed = {
-      'astro',
-      'asm',
-      'nasm',
-      'scheme',
-      'blueprint',
-      'qmljs',
-      -- 'latex',
-      'svelte',
-      'vue',
-      'c',
-      'lua',
-      'vim',
-      'fish',
-      'prisma',
-      'vimdoc',
-      'query',
-      'rust',
-      'cpp',
-      'fennel',
-      'http',
-      'nix',
-      'python',
-      'bash',
-      'typescript',
-      'tsx',
-      'proto',
-      'html',
-      'htmldjango',
-      'xml',
-      'javascript',
-      'typst',
-      'java',
-      'just',
-      'json',
-      'norg',
-      'cmake',
-      'dart',
-      'css',
-      'scss',
-      'diff',
-      'dockerfile',
-      'gdscript',
-      'go',
-      'solidity',
-      'gomod',
-      'gitcommit',
-      'git_rebase',
-      'git_config',
-      'php',
-      'luadoc',
-      'make',
-      'markdown_inline',
-      'nu',
-      'sql',
-      'kdl',
-      'toml',
-      'yaml',
-      'meson',
-      'zig',
-      'regex',
-      'gitignore',
-      'markdown',
-    },
-    highlight = { enable = true },
-    -- BUG:buggy :(
-    -- indent = { enable = true },
-    -- tree_setter = { enable = true },
-    incremental_selection = {
-      enable = true,
-      disable = { 'norg' },
-      keymaps = {
-        init_selection = '<CR>',
-        node_incremental = '<CR>',
-        node_decremental = '<BS>',
-        scope_incremental = '<TAB>',
-      },
-    },
-    auto_install = false,
-    sync_install = false,
-    textobjects = {
-      select = {
-        enable = true,
-
-        -- Automatically jump forward to textobjects, similar to targets.vim
-        lookahead = true,
-
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          -- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
-          ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-        },
-        -- You can choose the select mode (default is charwise 'v')
-        selection_modes = {
-          ['@parameter.outer'] = 'v', -- charwise
-          ['@function.outer'] = 'V', -- linewise
-          ['@class.outer'] = '<c-v>', -- blockwise
-        },
-        -- If you set this to `true` (default is `false`) then any textobject is
-        -- extended to include preceding or succeeding whitespace. Succeeding
-        -- whitespace has priority in order to act similarly to eg the built-in
-        -- `ap`. Can also be a function (see above).
-        include_surrounding_whitespace = true,
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']f'] = '@function.outer',
-          [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
-        },
-        goto_previous_start = {
-          ['[f'] = '@function.outer',
-          ['[z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          [']p'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['[p'] = '@parameter.inner',
-        },
-      },
-    },
-  }
-end
+local parsers = {
+  'astro',
+  'asm',
+  'nasm',
+  'scheme',
+  'blueprint',
+  'qmljs',
+  -- 'latex',
+  'svelte',
+  'vue',
+  'c',
+  'lua',
+  'vim',
+  'fish',
+  'prisma',
+  'vimdoc',
+  'query',
+  'rust',
+  'cpp',
+  'fennel',
+  'http',
+  'nix',
+  'python',
+  'bash',
+  'typescript',
+  'tsx',
+  'proto',
+  'html',
+  'htmldjango',
+  'xml',
+  'javascript',
+  'typst',
+  'java',
+  'just',
+  'json',
+  'norg',
+  'cmake',
+  'dart',
+  'css',
+  'scss',
+  'diff',
+  'dockerfile',
+  'gdscript',
+  'go',
+  'solidity',
+  'gomod',
+  'gitcommit',
+  'git_rebase',
+  'git_config',
+  'php',
+  'luadoc',
+  'make',
+  'markdown_inline',
+  'nu',
+  'sql',
+  'kdl',
+  'toml',
+  'yaml',
+  'meson',
+  'zig',
+  'regex',
+  'gitignore',
+  'markdown',
+}
 return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = config,
+    config = function()
+      require('nvim-treesitter').install(parsers)
+    end,
     event = { 'VeryLazy' },
+    branch = 'main',
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = parsers,
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
     dependencies = {
       'chrisgrieser/nvim-various-textobjs',
       'HiPhish/rainbow-delimiters.nvim',
-      -- { 'nushell/tree-sitter-nu', build = ':TSUpdate nu' },
       'nvim-treesitter/nvim-treesitter-context',
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
+        keys = {
+          {
+            'af',
+            function()
+              require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects')
+            end,
+            mode = { 'x', 'o' },
+          },
+          {
+            'if',
+            function()
+              require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects')
+            end,
+            mode = { 'x', 'o' },
+          },
+        },
+        opts = {
+          select = {
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true of false
+            include_surrounding_whitespace = false,
+          },
+        },
+      },
+      {
+        'daliusd/incr.nvim',
+        config = true,
+      },
     },
   },
   {
@@ -166,7 +153,6 @@ return {
       },
     },
   },
-
   {
     'Wansmer/treesj',
     keys = { { '<space>j', ':TSJToggle<CR>', desc = 'join or split' } },
